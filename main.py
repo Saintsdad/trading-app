b73c95b3faac50cb496e3cb4957c902418569ad6
Saintsdad <saintsrobot@gmail.com>
2024-12-28 10:19:03 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "App is running"}
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=port)
